ebcba124678a51b3b3cd2f45c47724b60e06d568
kwelch2 <kwelch@gemfireems.org>
2026-01-05 22:17:06 +0000

    e

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Payroll/",  // <--- Needs leading and trailing slashes
   plugins: [react()],
   server: {
     host: '0.0.0.0',
